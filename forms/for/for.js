9d357497219a8e3e31713d404ba93c7757e2455a
chc72331 <chc72331@creighton.edu>
2021-03-03 11:14:16 -0600

    Update for.js

--- forms/for/for.js
+++ forms/for/for.js
@@ -1,1 +1,4 @@
-// Please let this show up on the forBranch...
+let names = ['Bob', 'Janet', 'Tom', 'Erin', 'Lucy', 'Alyssa', 'Dustin']
+
+for (nombre = 0; nombre < names.length; nombre ++)
+  console.log(`The names on the list are ${names[nombre]}.`)
