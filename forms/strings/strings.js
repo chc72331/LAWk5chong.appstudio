d57ed302e530453113bebbbf93386131dc87d80a
chc72331 <chc72331@creighton.edu>
2021-03-03 14:44:22 -0600

    Create strings.js

---
+++ forms/strings/strings.js
@@ -0,0 +1,11 @@
+let quoteString = 'Failure is simply the opportunity to begin again this time more intelligently.'
+  
+let capsQuoteString = quoteString.toUpperCase()
+
+console.log(`Upper case string is: ${capsQuoteString}`)
+
+let authorString = '- Henry Ford'
+
+let completeString = quoteString.concat(authorString)
+
+console.log(`Concatenated string is: ${completeString}`)
