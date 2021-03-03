7b1cdeb7528bdecce23bb9c612670e50cd433c45
chc72331 <chc72331@creighton.edu>
2021-03-03 14:49:21 -0600

    Update strings.js

--- forms/strings/strings.js
+++ forms/strings/strings.js
@@ -9,3 +9,10 @@ let authorString = '- Henry Ford'
 let completeString = quoteString.concat(authorString)
 
 console.log(`Concatenated string is: ${completeString}`)
+
+let found = completeString.includes('Henry')
+
+if (found)
+  console.log('Henry was in completeString.')
+else
+  console.log('Henry was NOT in completeString.')
