897e929a105683b76965aef196bda7ab343c27d0
chc72331 <chc72331@creighton.edu>
2021-03-03 11:57:10 -0600

    Update for.js

--- forms/for/for.js
+++ forms/for/for.js
@@ -1,4 +1,6 @@
+// Establish an array full of names
 let names = ['Bob', 'Janet', 'Tom', 'Erin', 'Lucy', 'Alyssa', 'Dustin']
 
+// Create the for loop
 for (nombre = 0; nombre < names.length; nombre ++)
   console.log(`The names on the list are ${names[nombre]}.`)
