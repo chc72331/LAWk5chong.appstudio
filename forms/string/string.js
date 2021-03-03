befff4abb47a613c99ed1c157114063a29e01447
chc72331 <chc72331@creighton.edu>
2021-03-03 13:58:26 -0600

    Create string.js

---
+++ forms/string/string.js
@@ -0,0 +1,5 @@
+let quoteString = 'Failure is simply the opportunity to begin again this time more intelligently.'
+  
+capsQuoteString = quoteString.toUpperCase
+
+console.log(`Upper case string is: ${capsQuoteString})
