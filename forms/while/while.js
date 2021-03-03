1bcf206a1b6a752aab1e66e291f64598d9673141
chc72331 <chc72331@creighton.edu>
2021-03-03 14:09:19 -0600

    Update while.js

--- forms/while/while.js
+++ forms/while/while.js
@@ -1,8 +1,8 @@
 // Get a number input from a user
-numOfLoops = prompt('How many times do you want the loop to run?')
+let numOfLoops = prompt('How many times do you want the loop to run?')
 
 // Create while loop
-numpy = 0
+let numpy = 0
 
 while (numpy < numOfLoops) {
   console.log(`This is loop iteration ${numpy + 1}.`)
