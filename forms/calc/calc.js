d948316296bd39e71050678ec0d2438f5d5376b5
chc72331 <chc72331@creighton.edu>
2021-03-03 11:50:43 -0600

    Update calc.js

--- forms/calc/calc.js
+++ forms/calc/calc.js
@@ -3,8 +3,10 @@ function calculate(num1, num2) {
   return num1 + num2
 }
 
+// Get user input numbers
 userNum1 = prompt('Give me a number!')
 userNum2 = prompt('Give me another number!')
 
+// Take the parameters and run them through function
 let simpleMath = calculate(10, 3)
 console.log(`The sum of ${userNum1} and ${userNum2} is ${simpleMath}.`)
