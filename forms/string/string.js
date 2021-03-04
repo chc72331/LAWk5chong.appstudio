da5a46853eeef2ddc9af31dffc57318d9fe6253d
chc72331 <chc72331@creighton.edu>
2021-03-04 10:01:09 -0600

    Delete string.js

--- forms/string/string.js
+++
@@ -1,8 +0,0 @@
-let quoteString = 'Failure is simply the opportunity to begin again this time more intelligently.'
-  
-capsQuoteString = quoteString.toUpperCase
-
-console.log(`Upper case string is: ${capsQuoteString}`)
-
-authorString = '- Henry Ford"
-
